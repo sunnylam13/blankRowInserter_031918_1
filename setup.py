860b9728b1d6b2b1d3149a71ea66ba0251c446f7
Sunny Lam <sunny.lam@gmail.com>
2018-03-19 11:38:59 -0400

    revised files

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
 	'description': 'The program takes two integers and a filename string as command line arguments.  It then adds a number of rows based on the two integers provided.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
+	'url': 'https://github.com/sunnylam13/blankRowInserter_031918_1',
+	'download_url': 'https://github.com/sunnylam13/blankRowInserter_031918_1',
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
